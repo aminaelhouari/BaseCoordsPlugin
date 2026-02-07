55ca07bb7dc4ce951930abacb9cad149bbacec10
amina <amina.elhouari94@icloud.com>
2026-02-07 21:33:04 +0100

    Création de la commande /setbase

---
+++ src/main/java/fr/ikalaga/basecoords/CommandSetBase.java
@@ -0,0 +1,39 @@
+package fr.ikalaga.basecoords;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.Location;
+
+public class CommandSetBase implements CommandExecutor {
+    private final Main plugin;
+
+    public CommandSetBase(Main plugin){
+        this.plugin = plugin;
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Seul un joueur peut faire cela !");
+            return true;
+        }
+        Player player = (Player) sender;
+        Location loc = player.getLocation();
+        double x = loc.getX();
+        double y = loc.getY();
+        double z = loc.getZ();
+        if (args.length == 0){
+            player.sendMessage("Tu dois donner un nom à ta base! (/setbase [nom]");
+            return true;
+        }
+        String nomBase = args[0];
+        String path = "bases."+player.getUniqueId()+ "." + nomBase;
+        this.plugin.getConfig().set(path + ".x", x);
+        this.plugin.getConfig().set(path + ".y", y);
+        this.plugin.getConfig().set(path + ".z", z);
+        this.plugin.saveConfig();
+        player.sendMessage("§aBase" + nomBase + " enregistreee!");
+        return true;
+    }
+}
---
+++ src/main/java/fr/ikalaga/basecoords/Main.java
@@ -0,0 +1,12 @@
+package fr.ikalaga.basecoords;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Le plugin BaseCoords est chargé");
+        getCommand("setbase").setExecutor(new CommandSetBase(this));
+    }
+}
